0071c1e02fad2515dd05d3fcf910d4d82afd9839
Jili9855 <jili9855@uni.sydney.edu.au>
2022-06-04 20:53:38 +1000

    musicplayer

--- dist/index.f3bd186e.js
+++ dist/index.f3bd186e.js
@@ -11,7 +11,7 @@ window.addEventListener("load", ()=>{
 function loadMusic(indexNumb) {
     musicName.innerText = allMusic[indexNumb - 1].name;
     musicArtist.innerText = allMusic[indexNumb - 1].artist;
-    musicImg.src = `images/${allMusic[indexNumb - 1].src}.jpg`;
+    musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
     mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
 }
 //play music function
